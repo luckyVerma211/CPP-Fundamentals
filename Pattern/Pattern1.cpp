5098a3a351889824d9d2d57c237dbe6ea48d1eb4
Lucky Verma <109918194+luckyVerma211@users.noreply.github.com>
2023-04-18 11:02:07 +0530

    Create Pattern1.cpp

---
+++ Pattern/Pattern1.cpp
@@ -0,0 +1,21 @@
+/*
+54321
+5432
+543
+54
+5  */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of rows:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=i;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
